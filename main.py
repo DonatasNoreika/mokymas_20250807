18c3020cebf68bc115c5da5b9d96e9d6c162e627
DonatasNoreika <donoras@gmail.com>
2025-08-07 21:54:46 +0300

    Pagrindiniame projekto kataloge sukurti failą main.py
    Faile main.py importuoti Kursas modulį (failą)
    
    Faile main.py importuoti PythonKursas modulį (failą)
    
    Faile main.py inicijuoti Kursas objektą su norimomis savybėmis
    
    Faile main.py inicijuoti PythonKursas objektą su norimomis savybėmis
    
    Paleisti abiejų objektų metodą destyti()

---
+++ main.py
@@ -0,0 +1,8 @@
+from models.kursas import Kursas
+from models.python_kursas import PythonKursas
+
+kursas1 = Kursas("Fizika", "Joris", 200)
+kursas2 = PythonKursas("Pagrindai", "Donatas", 80)
+
+kursas1.destyti()
+kursas2.destyti()
